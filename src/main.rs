9f94d8433c1924684edce37c3be1238f8924a3b9
Ari Karhu <ari@baboonplanet.com>
2017-06-17 19:32:49 +0300

    ncurses init with loop and keybindings

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,22 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serial;
+extern crate ncurses;
 
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use serial::prelude::*;
+use ncurses::*;
+
+const COLOR_BACKGROUND: i16 = 16;
+const COLOR_FOREGROUND: i16 = 17;
+const COLOR_PAIR_DEFAULT: i16 = 1;
+
+const KEY_TOGGLE: i32 = 32;
+const KEY_MARKER: i32 = 10;
+const KEY_QUIT: i32 = 113;
 
 const SETTINGS: serial::PortSettings = serial::PortSettings {
     baud_rate:    serial::Baud115200,
@@ -40,7 +50,28 @@ fn main() {
     port.configure(&SETTINGS).unwrap();
     port.set_timeout(std::time::Duration::from_secs(3)).unwrap();
 
-    
+    initscr();
+    keypad(stdscr(), true);
+    nodelay(stdscr(), true);
+    noecho();
+
+    start_color();
+    init_color(COLOR_BACKGROUND, 0, 0, 0);
+    init_color(COLOR_FOREGROUND, 200, 300, 200);
+    init_pair(COLOR_PAIR_DEFAULT, COLOR_FOREGROUND, COLOR_BACKGROUND);
+
+    bkgd(' ' as chtype | COLOR_PAIR(COLOR_PAIR_DEFAULT) as chtype);
+
+    loop {
+        match getch() {
+            KEY_TOGGLE => println!("TOGGLE!"),
+            KEY_MARKER => println!("MARKER!"),
+            KEY_QUIT => break,
+            _ => ()
+        }
+    }
+
+    endwin();
 }
 
 #[cfg(windows)]
