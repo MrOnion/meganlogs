34da2a385f921b2d9fc95364f58e23b17f86856d
Ari Karhu <ari@baboonplanet.com>
2017-06-17 15:12:09 +0300

    simple serial port config

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@ extern crate toml;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate serial;
 
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use serial::prelude::*;
+
+const SETTINGS: serial::PortSettings = serial::PortSettings {
+    baud_rate:    serial::Baud115200,
+    char_size:    serial::Bits8,
+    parity:       serial::ParityNone,
+    stop_bits:    serial::Stop1,
+    flow_control: serial::FlowNone
+};
 
 #[derive(Deserialize)]
 struct Config {
@@ -24,7 +34,13 @@ fn main() {
     config_file.read_to_string(&mut config_data).unwrap();
     let config: Config = toml::from_str(&config_data).unwrap();
 
-    println!("{:?} {:?} {:?} {:?} {:?}", config.serial_device, config.log_path, config.data_rate, config.split, config.prefix);
+    let a = "/dev/ttyUSB0".to_string();
+    let device: &String = config.serial_device.as_ref().unwrap_or(&a);
+    let mut port: serial::SystemPort = serial::open(device).unwrap();
+    port.configure(&SETTINGS).unwrap();
+    port.set_timeout(std::time::Duration::from_secs(3)).unwrap();
+
+    
 }
 
 #[cfg(windows)]
