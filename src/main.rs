fdb2fcc8bc9be405a0acb424fb4c91e939c49240
Ari Karhu <ari@baboonplanet.com>
2017-06-22 00:16:46 +0300

    add colored text bar for showing the logging status

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,8 @@ use std::path::Path;
 use serial::prelude::*;
 use ncurses::*;
 
-const COLOR_BACKGROUND: i16 = 16;
-const COLOR_FOREGROUND: i16 = 17;
-const COLOR_PAIR_DEFAULT: i16 = 1;
+const COLOR_PAIR_OFF: i16 = 1;
+const COLOR_PAIR_ON: i16 = 2;
 
 const KEY_TOGGLE: i32 = 32;
 const KEY_MARKER: i32 = 10;
@@ -39,8 +38,19 @@ struct Config {
     prefix: Option<String>
 }
 
+fn change_status(state: bool) {
+    attron(A_BOLD() | COLOR_PAIR(if state {COLOR_PAIR_ON} else {COLOR_PAIR_OFF}));
+    let width = getmaxx(stdscr()) - 1;
+    (1..width).fold((), |_, x| {
+        let z = mvaddch(7, x, 32);
+    });
+    mvaddstr(7, (width - 3) / 2, (if state { "ON" } else { "OFF" }));
+}
+
 #[cfg(unix)]
 fn main() {
+    let mut logging = false;
+
     let mut config_file: File = File::open(Path::new("meganlogs.toml")).unwrap();
     let mut config_data: String = String::new();
     config_file.read_to_string(&mut config_data).unwrap();
@@ -56,8 +66,6 @@ fn main() {
     let sig_firmware: String = command::signature_firmware(&mut port).unwrap();
     let sig_comms: String = command::signature_comms(&mut port).unwrap();
 
-    println!("{:?}", sig_comms);
-
     initscr();
     keypad(stdscr(), true);
     nodelay(stdscr(), true);
@@ -66,21 +74,27 @@ fn main() {
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
     start_color();
-    init_color(COLOR_BACKGROUND, 0, 0, 0);
-    init_color(COLOR_FOREGROUND, 100, 300, 200);
-    init_pair(COLOR_PAIR_DEFAULT, COLOR_FOREGROUND, COLOR_BACKGROUND);
+    init_pair(COLOR_PAIR_OFF, COLOR_WHITE, COLOR_RED);
+    init_pair(COLOR_PAIR_ON, COLOR_WHITE, COLOR_GREEN);
 
     box_(stdscr(), 0, 0);
 
     mvaddstr(1, 1, "*** MeganLogs ***");
-    mvaddstr(3, 1, "Firmware: ");
-    mvaddstr(3, 12, &sig_firmware);
-    mvaddstr(4, 1, "Comms:    ");
-    mvaddstr(4, 12, &sig_comms);
+    mvaddstr(3, 1, "Serial:");
+    mvaddstr(3, 12, device);
+    mvaddstr(4, 1, "Firmware:");
+    mvaddstr(4, 12, &sig_firmware);
+    mvaddstr(5, 1, "Comms:");
+    mvaddstr(5, 12, &sig_comms);
+
+    change_status(logging);
 
     loop {
         match getch() {
-            KEY_TOGGLE => println!("TOGGLE!"),
+            KEY_TOGGLE => {
+                logging = !logging;
+                change_status(logging);
+            },
             KEY_MARKER => println!("MARKER!"),
             KEY_QUIT => break,
             _ => ()
